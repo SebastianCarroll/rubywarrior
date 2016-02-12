bc95a633b8f17369099447adb00bb11ced246f03
Sebastian Carroll <seabass25@hotmail.com>
2016-02-12 18:22:27 +1100

    Implemented state machine and passed level 3

--- fleue-intermediate/player.rb
+++ fleue-intermediate/player.rb
@@ -28,67 +28,63 @@ class Player
   def act
     case @state
     when :normal
-      # do normal stuff
-      # What if this isn't the right state? How do I transition and then act in one movement? In level 3 I start in :normal state but want to immediately bind a slime. I have the decision extracted to a method so can recurse here? Seems like i could get myself in a mess as I can only act once per turn. I don't see any other way to do it without duplicating logic.
-
-
-      move_to_exit
+      act_normal
     when :fight
-      # Attack things
-      puts @state
-      defeat_slimes
+      attack_slime
+    when :bind
+      bind_slimes
     when :free
       # Help those bros
-      puts @state
     when :heal
-      # Better check yo self
-      puts @state
+      if @warrior.health <= 18
+        rest
+      else
+        move_to_exit
+        transition(:normal)
+      end
     else
       puts @state
     end
-
-    enemies = @directions.select{|d| @warrior.feel(d).enemy?}
   end
 
+  def act_normal
+    if @enemies.empty?
+      if @friendly_captives.empty?
+        if @captives.empty?
+          move_to_exit
+        else
+          attack_slime
+          transition(:fight)
+        end
+      else
+        free_captive
+      end
+    else
+      @warrior.bind! @enemies.pop
+    end
+  end
 
-    # TODO: This decision tree is getting complex. How to break it down into more simple objects? Could use a state machine but not sure exactly how that would work or if it would even give me what i want.
-    #if enemies.empty?
-    #  if @friendly_captives.empty?
-    #    # Kill other captives
-    #    captives = @directions.select{|d| @warrior.feel(d).captive?}
-    #    if captives.empty?
-    #      puts @state
-    #      # Normal state
-    #    else
-    #      puts @state
-    #      # kill captives state
-    #    end
-    #  else
-    #    # Free actual captives
-    #    puts @state
-    #  end
-#
-#      if @healing
-#        if @warrior.health == 20
-#          # Fully healed
-#          @healing = false
-#        else
-#          # Still healing and safe to do so
-#          @warrior.rest!
-#        end
-#      else
-#        # Fully healed and no monstors so look for exit
-#        @warrior.walk! @warrior.direction_of_stairs
-#      end
-#    else
-#      if @warrior.health < 5
-#        @healing = true
-#        retreat(enemies)
-#      else
-#        @warrior.bind! enemies.pop
-#      end
-#    end
-#  end
+  def free_captive
+    @warrior.rescue! @friendly_captives.pop
+  end
+
+  def attack_slime
+    if @enemies.empty? && @warrior.health < 7
+      rest
+    elsif !@enemies.empty?
+      @warrior.attack! @enemies.pop
+    elsif ! @captives.empty?
+      @warrior.attack! @captives.pop
+    else
+      move_to_exit
+      transition(:normal)
+    end
+  end
+
+  def rest
+    @warrior.rest!
+    transition(:heal)
+  end
 
   # Moves warrior away from enemies
   def retreat(enemies)
@@ -101,27 +97,26 @@ class Player
   end
 
   def move_to_exit
-    enemies = @directions.select{|d| @warrior.feel(d).enemy?}
-    if enemies.empty?
-      @warrior.walk! @warrior.direction_of_stairs
-    else
-      transition(:fight) 
-    end
+    @warrior.walk! @warrior.direction_of_stairs
   end
 
-  def defeat_slimes
+  def bind_slimes
     enemies = @directions.select{|d| @warrior.feel(d).enemy?}
+    captives = @directions.select{|d| @warrior.feel(d).captive?}
     if enemies.empty?
-      transition(:normal)
+      if @friendly_captives.empty?
+        transtition(:free)
+      elsif captives.empty?
+        transition(:normal)
+      else
+      end
     else
       @warrior.bind! enemies.pop
     end
-
   end
 
   # Transition to the new state and restart the decision logic
   def transition(state)
     @state = state
-    act
   end
 end
