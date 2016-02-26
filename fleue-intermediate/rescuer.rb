051f78418f8bc06afd5b2a4ef7c4bd412d3c2169
Sebastian Carroll <seabass25@hotmail.com>
2016-02-26 18:17:47 +1100

    Refactored rescuer

--- fleue-intermediate/rescuer.rb
+++ fleue-intermediate/rescuer.rb
@@ -2,23 +2,15 @@ module Rescuer
   def free_captives
     transition(:free)
     if adjacent_to?(:enemy?)
-      if @d_captives.any?{|d| d.ticking?}
+      if can_hear?(:ticking?)
         bind_slimes
       else
         attack_slime
       end
     elsif adjacent_to?(:captive?)
       free_captive
-    elsif !@d_captives.empty?
-      dir = @warrior.direction_of(@d_captives.pop)
-      stairs = @warrior.direction_of_stairs
-      to_walk = if @warrior.feel(dir).stairs?
-                  @directions.shuffle.detect{|s| s != dir && @warrior.feel(s).empty?}
-                else
-                  dir
-                end
-
-      @warrior.walk! to_walk
+    elsif can_hear?(:captive?)
+      move_to_captive
     else
       move_to_exit
     end
@@ -28,4 +20,23 @@ module Rescuer
     @warrior.rescue! @captives.first
   end
 
+  def move_to_captive
+    dir = @warrior.direction_of(@d_captives.pop)
+    avoid_if(dir, :stairs?)
+  end
+
+  # Avoid dir if test passes on that direction
+  def avoid_if(dir, test)
+    free_spots = @directions.select{|d| @warrior.feel(d).empty?}
+
+    to_walk = 
+      if free_spots.include?(dir) && !@warrior.feel(dir).send(test)
+        dir
+      else
+        free_spots.delete(dir)
+        free_spots.first
+      end
+
+    @warrior.walk! to_walk
+  end
 end
