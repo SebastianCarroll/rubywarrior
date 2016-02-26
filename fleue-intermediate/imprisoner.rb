85d3e84082c3833743dc422b3f598d243233a59f
Sebastian Carroll <seabass25@hotmail.com>
2016-02-26 16:27:57 +1100

    Refactored imprisoner medule

--- fleue-intermediate/imprisoner.rb
+++ fleue-intermediate/imprisoner.rb
@@ -2,16 +2,18 @@ module Imprisoner
   def bind_slimes
     enemies = @directions.select{|d| @warrior.feel(d).enemy?}
     captives = @directions.select{|d| @warrior.feel(d).captive?}
-    if not adjacent_to?(:enemy?)
-      if @friendly_captives.empty?
-        transtition(:free)
-      elsif not adjacent_to?(:captive?)
-        #transition(:normal)
-        move_to_exit
-      else
-      end
+    if adjacent_to?(:enemy?)
+      bind_enemy
     else
-      @warrior.bind! enemies.pop
+      if adjacent_to?(:captive?)
+        free_captives
+      else
+        move_to_exit
+      end
     end
   end
+
+    def bind_enemy
+      @warrior.bind! @enemies.pop
+    end
 end
